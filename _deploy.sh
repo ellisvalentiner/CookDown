a452a292ced2e01dd3f4e336ba9591bc413609fa
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-03-03 10:40:34 -0500

    attempt Travis deploy fix

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && ![ "$TRAVIS_PULL_REQUEST" ] && exit 0
 
 git config --global user.email "ellisvalentiner@gmail.com"
 git config --global user.name "Ellis Valentiner"
@@ -12,5 +12,5 @@ git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git b
 cd book-output
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m "Update the cook book" || true
 git push origin gh-pages
