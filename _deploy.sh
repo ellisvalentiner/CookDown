10f4cb692256d58366afae695e6a7d4f0ca597c3
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-03-03 11:09:50 -0500

    second attempt

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && ![ "$TRAVIS_PULL_REQUEST" ] && exit 0
+[ "${TRAVIS_BRANCH}" = "master" ] && ![ "$TRAVIS_PULL_REQUEST" ] && exit 0
 
 git config --global user.email "ellisvalentiner@gmail.com"
 git config --global user.name "Ellis Valentiner"
